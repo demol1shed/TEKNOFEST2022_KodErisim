3ccb289845d0488cd1a4dbf32168a818db09fd77
demol1shed <cosardemirarda@gmail.com>
2022-04-13 23:17:28 +0300

    4.13.22_23
    - 2 yeni kitaplık oluşturuldu(MZ80, BTS7960).
    - Bilinen bug => <filename.h> no such file or directory found.

--- main.ino
+++ main.ino
@@ -1,30 +1,14 @@
-// her pc'de çalışmayacak.
-#include "C:\Users\user\Documents\Arduino\libraries\Custom_Headers\src\BTS7960B.h"
-#include "C:\Users\user\Documents\Arduino\libraries\Custom_Headers\src\MZ80.h"
-#include <Arduino.h>
-//#include <Q2HX711.h>
+#include <BTS7960B.h>
+#include <MZ80.h>
 
-#define MZ80_CON 10 // MZ80 pin
-#define LED_ARD 13  // MZ80 uyacıcı led
-
-#define DATA 3
-#define CLK 4
-
-MZ80 optikSensor;
-BTS7960B motor;
+MZ80 mz;
+BTS7960B bts;
 
 void setup(){
     Serial.begin(9600);
-    motor.BTS_SETUP();
-    optikSensor.MZ80_SETUP(MZ80_CON);
 }
 
 void loop(){
-    bool veri = optikSensor.MZ80_INIT(MZ80_CON);
-    if(veri){
-        motor.CCLKWTURN(0);
-    }else{
-        motor.CCLKWTURN(255);
-    }
-    //delay(50);
+    Serial.println("Hello world.");
+    delay(500);
 }
