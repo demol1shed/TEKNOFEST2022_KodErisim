a2666f80d757952321ee6a0cfc61099ac3fa256b
demol1shed <cosardemirarda@gmail.com>
2022-04-14 23:52:17 +0300

    14.4.22_23

--- main.ino
+++ main.ino
@@ -246,7 +246,7 @@ int vectoralPayloads[2] = {0,0};
 }*/
 #pragma endregion
 
-#include <nRF.h>
+/*#include <nRF.h>
 #include <joystick.h>
 
 uint8_t address[][6] = {"1Node", "2Node"};
@@ -260,4 +260,48 @@ void setup(){
 
 void loop(){
 
+}*/
+
+#include <Wire.h>    
+ 
+#define disk1 0x57    //Address of 24LC256 eeprom chip
+ 
+void setup(void)
+{
+  Serial.begin(9600);
+  Wire.begin();  
+ 
+  unsigned int address = 0;
+ 
+  writeEEPROM(disk1, address, 123);
+  Serial.print(readEEPROM(disk1, address), DEC);
+}
+ 
+void loop(){}
+ 
+void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data ) 
+{
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+ 
+  delay(5);
+}
+ 
+byte readEEPROM(int deviceaddress, unsigned int eeaddress ) 
+{
+  byte rdata = 0xFF;
+ 
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.endTransmission();
+ 
+  Wire.requestFrom(deviceaddress,1);
+ 
+  if (Wire.available()) rdata = Wire.read();
+ 
+  return rdata;
 }
