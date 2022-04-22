c00f23ba342ecded23d04f00f79efd5ca75d5fb6
demol1shed <cosardemirarda@gmail.com>
2022-04-22 11:26:46 +0300

    22.4.22_initial commit

--- main.ino
+++ main.ino
@@ -1,36 +1,42 @@
 #include <nRF24.h>
 #include <SPI.h>
 #include <RF24.h>
-#include <U8glib.h>
-#include <ekran.h>
+#include <BTS7960B.h>
 
 
-#define CE 9
-#define CSN 8
+#define RPWM 13
+#define LPWM 12
+#define RPWM2 11
+#define LPWM2 10
+
 // ALICI PARÇA
-const int ekranSayisi = 2;
-unsigned int mesajlar[5];
-byte durumlar[ekranSayisi][3] = {{LOW,LOW,LOW}, {HIGH,LOW,LOW}};
-OledEkran ekranModul(0, 20);
-U8GLIB_SSD1306_128X64 ekran(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
-U8GLIB_SSD1306_128X64 ekran1(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
-U8GLIB_SSD1306_128X64 ekranlar[ekranSayisi]{
-  ekran, 
-  ekran1
-};
-RF24 radio(CE, CSN);
-nRF24 radioModule;
+BTS7960B motor(RPWM, LPWM);
+BTS7960B motor2(RPWM2, LPWM2);
+// RF24 radio(CE, CSN);
+// nRF24 radioModule;
+int durum = 0;
 
 void setup(){
-  radio = radioModule.nRF24ReceiverSetup(radio, RF24_PA_MIN, 9600, RF24_1MBPS);
+  Serial.begin(9600);
+  // radio = radioModule.nRF24ReceiverSetup(radio, RF24_PA_MIN, 9600, RF24_250KBPS);
 }
 
 void loop(){
-  radioModule.nRF24Receive(radio);
-  ekranModul.SetDisp(durumlar[0]);
-  ekranModul.WriteToScreen(ekranlar[0], radioModule.receivingValues);
+  // radioModule.nRF24Receive(radio);
+  //Serial.println(radioModule.receivingValues[0]);
+  unsigned long started = millis();
+  while(millis() - started < 2000){
+    motor.CCLKWTURN(255);
+    motor2.CCLKWTURN(255);
+    //Serial.println("cclockwise");
+    //Serial.println("cclockwise");
+  }
+  unsigned long started2 = millis();
+  while(millis() - started2 < 5000){
+    motor.CLKWTURN(255);
+    motor2.CLKWTURN(255);
+  }
 }
-
 
 /*// Inclusão das Bibliotecas
 #include <Wire.h>
@@ -41,27 +47,24 @@ void loop(){
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
 U8GLIB_SSD1306_128X64 u8g1(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
 U8GLIB_SSD1306_128X64 u8g2(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
-// Define e Configura Display
+
 
 char bufferX [20];
 char bufferY [20];
 char bufferZ [20];
 
 
-// Definição de Pinos
+
 const int pinAddA = 5;
 const int pinAddB = 6;
 const int pinAddC = 7;
 
 void setup() {
-  
-  // Inicializa pinos
+
   pinMode(pinAddA, OUTPUT);
   pinMode(pinAddB, OUTPUT);
   pinMode(pinAddC, OUTPUT);
-  
 
-  // Inicializa Displays no endereço 0x3C do I2C
   Wire.begin();
 
 }
@@ -69,18 +72,18 @@ void setup() {
 void loop() {
   selDisp1();
 
-  //sprintf(bufferX, "Robolink Akademi");
-  sprintf(bufferY, "kod");
-  //sprintf(bufferZ, "    Example");
+  sprintf(bufferX, "Robolink Akademi");
+  sprintf(bufferY, "sik");
+  sprintf(bufferZ, "Example");
   u8g.firstPage();
   do {
     draw();
   } while ( u8g.nextPage() );
 
   selDisp2();
- // sprintf(bufferX, "Osman Erşahin");
-  sprintf(bufferY, "erisim");
-  //sprintf(bufferZ, "    sadasfasf");
+  sprintf(bufferX, "Osman Ersahin");
+  sprintf(bufferY, "durum");
+  sprintf(bufferZ, "    sadasfasf");
   u8g1.firstPage();
   do {
     draw1();
@@ -88,9 +91,9 @@ void loop() {
 
   selDisp3();
 
- // sprintf(bufferX, "iklim asoıfhasf");
+  sprintf(bufferX, "iklim asoifhasf");
   sprintf(bufferY, "2022");
- // sprintf(bufferZ, "    fgege");
+  sprintf(bufferZ, "    fgege");
   u8g2.firstPage();
   do {
     draw2();
@@ -145,31 +148,28 @@ void draw2(void)
 }*/
 #pragma region ekran
 /*#include <ekran.h>
-#include <U8glib.h>
+#include "U8glib.h"
 
 const int ekranSayisi = 3;
-const char* mesajlar[5] = {"kod", "erisim", "kod erisim"};
+const char* mesajlar[3] = {"kod", "erisim", "kod erisim"};
 byte durumlar[3][ekranSayisi] = {{LOW,LOW,LOW}, {HIGH,LOW,LOW}, {LOW,HIGH,LOW}};
 OledEkran ekranModul(0, 20);
 
 U8GLIB_SSD1306_128X64 ekran(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
 U8GLIB_SSD1306_128X64 ekran1(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
-U8GLIB_SSD1306_128X64 ekran2(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
-U8GLIB_SSD1306_128X64 ekranlar[ekranSayisi]{
-  ekran, 
-  ekran1, 
-  ekran2
-};
+
 
 void setup(){
   ekranModul.SetupPins();
+  // flip screen, if required
+  ekran.setRot180();
+  ekran1.setRot180();
 }
 
 void loop(){
-  for(int i = 0; i < ekranSayisi; i++){
-    ekranModul.SetDisp(durumlar[i]);
-    ekranModul.WriteToScreen(ekranlar[i], mesajlar);
-  }
+  ekranModul.SetDisp(durumlar[1]);
+  ekranModul.WriteToScreen(ekran, mesajlar);
+  ekranModul.SetDisp(durumlar[0]);
+  ekranModul.WriteToScreen(ekran1, mesajlar);
 }*/
 #pragma endregion
-
